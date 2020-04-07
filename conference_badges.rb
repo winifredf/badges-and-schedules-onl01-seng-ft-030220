c1260d6f33076c62d93fb511fb4a943e081ff788
Winifred Frierson <winifred.dykes@hotmail.com>
2020-04-07 17:33:36 +0000

    Automatically backed up by Learn

--- conference_badges.rb
+++ conference_badges.rb
@@ -17,10 +17,10 @@ def assign_rooms(name)
 
 def printer(name)
   batch_badge_creator(name).each do |badges|
-    puts #{badges}
+    puts "#{badges}"
   end
   assign_rooms(name).each do |room_assignment|
-    puts room_assignment
+    puts "#{room_assignment}"
   end
 end
 
