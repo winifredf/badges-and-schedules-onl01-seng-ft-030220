3199dbf605244ca4a450512d9fa7594f4126b25c
Winifred Frierson <winifred.dykes@hotmail.com>
2020-04-07 14:00:58 +0000

    Automatically backed up by Learn

--- conference_badges.rb
+++ conference_badges.rb
@@ -1,1 +1,27 @@
+def badge_maker(name)
+  return("Hello, my name is #{name}.")
+end
 # Write your code here.
+
+def batch_badge_creator(array)
+  new_array = []
+  array.each{|name| new_array << "Hello, my name is #{name}."}
+  new_array
+
+def assign_rooms(name)
+  rooms = []
+  name.each.with_index(1) do |name, room_assignment|
+    rooms << room_assignments = "Hello, #{name}! You'll be assigned to room #{room_assignment}!"
+  end
+  rooms
+
+def printer(name)
+  batch_badge_creator(name).each do |badges|
+    puts badges
+  end
+  assign_rooms(name).each do |room_assignment|
+    puts room_assignment
+  end
+end
+
+
